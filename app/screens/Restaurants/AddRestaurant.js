d5351dcd7711823a54aa0fb8b6a266385142c596
Agustin Navarro <xAgustin93@gmail.com>
2019-05-30 23:03:57 +0200

    Creando formulario de nuevo restaurante

--- app/screens/Restaurants/AddRestaurant.js
+++ app/screens/Restaurants/AddRestaurant.js
@@ -1,11 +1,125 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Icon, Image, Button } from "react-native-elements";
+import { Permissions, ImagePicker } from "expo";
+import Toast, { DURATION } from "react-native-easy-toast";
+
+import t from "tcomb-form-native";
+const Form = t.form.Form;
+import {
+  AddRestaurantStruct,
+  AddRestaurantOptions
+} from "../../forms/AddRestaurant";
 
 export default class AddRestaurant extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      imageUriRestaurant: "",
+      formData: {
+        name: "",
+        city: "",
+        address: "",
+        description: ""
+      }
+    };
+  }
+
+  isImageRestaurant = image => {
+    if (image) {
+      return (
+        <Image source={{ uri: image }} style={{ width: 500, height: 200 }} />
+      );
+    } else {
+      return (
+        <Image
+          source={require("../../../assets/img/no-image.png")}
+          style={{ width: 200, height: 200 }}
+        />
+      );
+    }
+  };
+
+  uploadImage = async () => {
+    const resultPermission = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+
+    if (resultPermission.status === "denied") {
+      this.refs.toast.show(
+        "Es necesario aceptar los permisos de la galeria",
+        1500
+      );
+    } else {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true
+      });
+
+      if (result.cancelled) {
+        this.refs.toast.show("Has cerrado la galeria de imagenes", 1500);
+      } else {
+        this.setState({
+          imageUriRestaurant: result.uri
+        });
+      }
+    }
+  };
+
+  onChangeFormAddRestaurant = formValue => {
+    this.setState({
+      formData: formValue
+    });
+  };
+
+  addRestaurant = () => {
+    console.log("Estamos enla funcion AddRestaurant");
+    console.log(this.state);
+  };
+
   render() {
+    const { imageUriRestaurant } = this.state;
+
     return (
       <View style={styles.viewBody}>
-        <Text>Add Restaurant Screen...</Text>
+        <View style={styles.viewPhoto}>
+          {this.isImageRestaurant(imageUriRestaurant)}
+        </View>
+        <View>
+          <Form
+            ref="addRestaurantForm"
+            type={AddRestaurantStruct}
+            options={AddRestaurantOptions}
+            value={this.state.formData}
+            onChange={formValue => this.onChangeFormAddRestaurant(formValue)}
+          />
+        </View>
+        <View style={styles.viewIconUploadPhoto}>
+          <Icon
+            name="camera"
+            type="material-community"
+            color="#7A7A7A"
+            iconStyle={styles.addPhotoIcon}
+            onPress={() => this.uploadImage()}
+          />
+        </View>
+        <View style={styles.viewBtnAddRestaurant}>
+          <Button
+            title="Crear Restaurante"
+            onPress={() => this.addRestaurant()}
+            buttonStyle={styles.btnAddRestaurant}
+          />
+        </View>
+
+        <Toast
+          ref="toast"
+          position="bottom"
+          positionValue={320}
+          fadeInDuration={1000}
+          fadeOutDuration={1000}
+          opacity={0.8}
+          textStyle={{ color: "#fff" }}
+        />
       </View>
     );
   }
@@ -13,9 +127,30 @@ export default class AddRestaurant extends Component {
 
 const styles = StyleSheet.create({
   viewBody: {
-    flex: 1,
+    flex: 1
+  },
+  viewPhoto: {
     alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff"
+    height: 200,
+    marginBottom: 20
+  },
+  viewIconUploadPhoto: {
+    flex: 1,
+    alignItems: "flex-start",
+    marginLeft: 12
+  },
+  addPhotoIcon: {
+    backgroundColor: "#e3e3e3",
+    padding: 17,
+    paddingBottom: 14,
+    margin: 0
+  },
+  viewBtnAddRestaurant: {
+    flex: 1,
+    justifyContent: "flex-end"
+  },
+  btnAddRestaurant: {
+    backgroundColor: "#00a680",
+    margin: 20
   }
 });
