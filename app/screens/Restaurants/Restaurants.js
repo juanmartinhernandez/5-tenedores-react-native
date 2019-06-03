e97a0375ec4562d045d6735cc1fb650210cde18d
Agustin Navarro <xAgustin93@gmail.com>
2019-06-04 00:19:48 +0200

    Ocultando el (+) de los usuarios no logeados

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -3,20 +3,59 @@ import { StyleSheet, View, Text } from "react-native";
 
 import ActionButton from "react-native-action-button";
 
+import * as firebase from "firebase";
+
 export default class Restaurants extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      login: false
+    };
+  }
+
+  componentDidMount() {
+    this.checkLogin();
+  }
+
+  checkLogin = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          login: true
+        });
+      } else {
+        this.setState({
+          login: false
+        });
+      }
+    });
+  };
+
   goToScreen = nameScreen => {
     this.props.navigation.navigate(nameScreen);
+  };
+
+  loadActionButton = () => {
+    const { login } = this.state;
+
+    if (login) {
+      return (
+        <ActionButton
+          buttonColor="#00a680"
+          onPress={() => this.goToScreen("AddRestaurant")}
+        />
+      );
+    }
+
+    return null;
   };
 
   render() {
     return (
       <View style={styles.viewBody}>
         <Text>Restaurants Screen...</Text>
-
-        <ActionButton
-          buttonColor="#00a680"
-          onPress={() => this.goToScreen("AddRestaurant")}
-        />
+        {this.loadActionButton()}
       </View>
     );
   }
