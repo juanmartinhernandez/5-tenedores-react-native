95aa55cc3cc7b5767cf09ba247c2567b88e6a8d0
Agustin Navarro <xAgustin93@gmail.com>
2019-06-17 23:46:56 +0200

    Pintando Lista de top 5 restaurantes

--- app/screens/TopFive.js
+++ app/screens/TopFive.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView
+} from "react-native";
+import { Card, Image, Rating } from "react-native-elements";
 
 import { firebaseApp } from "../utils/FireBase";
 import firebase from "firebase/app";
@@ -39,20 +46,77 @@ export default class TopFive extends Component {
     });
   };
 
+  renderRestaurants = restaurants => {
+    if (restaurants) {
+      return (
+        <View>
+          {restaurants.map((restaurant, index) => {
+            return (
+              <Card key={index}>
+                <Image
+                  style={styles.restaurantImage}
+                  resizeMode="cover"
+                  source={{ uri: restaurant.image }}
+                />
+                <View style={styles.titleRating}>
+                  <Text style={styles.title}>{restaurant.name}</Text>
+                  <Rating
+                    imageSize={20}
+                    startingValue={restaurant.rating}
+                    readonly
+                    style={styles.rating}
+                  />
+                </View>
+                <Text style={styles.description}>{restaurant.description}</Text>
+              </Card>
+            );
+          })}
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <ActivityIndicator size="large" />
+          <Text>Cargando restaurantes</Text>
+        </View>
+      );
+    }
+  };
+
   render() {
+    const { restaurants } = this.state;
+
     return (
-      <View style={styles.viewBody}>
-        <Text>Top Five Screen...</Text>
-      </View>
+      <ScrollView style={styles.viewBody}>
+        {this.renderRestaurants(restaurants)}
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   viewBody: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff"
+    flex: 1
+  },
+  restaurantImage: {
+    width: "100%",
+    height: 200
+  },
+  titleRating: {
+    flexDirection: "row",
+    marginTop: 10
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  rating: {
+    position: "absolute",
+    right: 0
+  },
+  description: {
+    color: "grey",
+    marginTop: 10,
+    textAlign: "justify"
   }
 });
